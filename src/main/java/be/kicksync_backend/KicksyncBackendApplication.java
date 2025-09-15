51f54ba185bff33b91e15c5432bbec9b444e7f62
gminnimk <tvt1245@naver.com>
2025-09-16 07:36:11 +0900

    init commit

---
+++ src/main/java/be/kicksync_backend/KicksyncBackendApplication.java
@@ -0,0 +1,13 @@
+package be.kicksync_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KicksyncBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KicksyncBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/kicksync_backend/KicksyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kicksync_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KicksyncBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
