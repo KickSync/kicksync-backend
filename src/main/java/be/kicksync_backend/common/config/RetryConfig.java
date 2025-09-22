08b7f6cf86226603cb3019f5879deab32f0c7895
gminnimk <tvt1245@naver.com>
2025-09-22 20:51:00 +0900

    feat: Spring Retry 설정으로 결제 실패 시 재시도 설정 추가 및 데이터 정합성 문제 해결

---
+++ src/main/java/be/kicksync_backend/common/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package be.kicksync_backend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+}
--- src/main/java/be/kicksync_backend/feature/payment/service/PaymentService.java
+++ src/main/java/be/kicksync_backend/feature/payment/service/PaymentService.java
@@ -4,49 +4,33 @@ import be.kicksync_backend.common.exception.CustomException;
 import be.kicksync_backend.common.exception.ErrorCode;
 import be.kicksync_backend.feature.order.entity.Order;
 import be.kicksync_backend.feature.order.repository.OrderRepository;
+import be.kicksync_backend.feature.payment.util.PaymentClient;
 import be.kicksync_backend.feature.payment.dto.PaymentCancelRequestDto;
 import be.kicksync_backend.feature.payment.dto.PaymentRequestDto;
 import be.kicksync_backend.feature.payment.entity.Payment;
 import be.kicksync_backend.feature.payment.repository.PaymentRepository;
-import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
-import com.siot.IamportRestClient.request.CancelData;
 import com.siot.IamportRestClient.response.IamportResponse;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import java.io.IOException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.List;
 import java.util.Objects;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final OrderRepository orderRepository;
-    private IamportClient iamportClient;
+    private final PaymentClient paymentClient;
+    private final PaymentTransactionService paymentTransactionService;
 
-    @Value("${iamport.api.key}")
-    private String apiKey;
-
-    @Value("${iamport.api.secret}")
-    private String apiSecret;
-
-    @PostConstruct
-    public void init() {
-        this.iamportClient = new IamportClient(apiKey, apiSecret);
-    }
-
-    @Transactional
     public Payment verifyPayment(PaymentRequestDto requestDto) throws IamportResponseException, IOException {
-        IamportResponse<com.siot.IamportRestClient.response.Payment> iamportResponse = iamportClient.paymentByImpUid(requestDto.getImpUid());
+        IamportResponse<com.siot.IamportRestClient.response.Payment> iamportResponse = paymentClient.getPaymentInfoByImpUid(requestDto.getImpUid());
         com.siot.IamportRestClient.response.Payment paymentInfo = iamportResponse.getResponse();
 
         Order order = orderRepository.findById(requestDto.getOrderId())
@@ -56,24 +40,9 @@ public class PaymentService {
             throw new CustomException(ErrorCode.PAYMENT_AMOUNT_MISMATCH);
         }
 
-        Payment payment = Payment.builder()
-                .userId(order.getUser().getId())
-                .orderId(order.getId())
-                .paymentAmount(paymentInfo.getAmount())
-                .paymentDate(LocalDateTime.ofInstant(Instant.ofEpochSecond(paymentInfo.getPaidAt().getTime() / 1000), ZoneId.systemDefault()))
-                .impUid(paymentInfo.getImpUid())
-                .paymentMethod(paymentInfo.getPayMethod())
-                .merchantUid(paymentInfo.getMerchantUid())
-                .pgProvider(paymentInfo.getPgProvider())
-                .pgTid(paymentInfo.getPgTid())
-                .status(paymentInfo.getStatus())
-                .cardName(paymentInfo.getCardName())
-                .cardNumber(paymentInfo.getCardNumber())
-                .build();
-        return paymentRepository.save(payment);
+        return paymentTransactionService.savePaymentRecord(paymentInfo, order);
     }
 
-    @Transactional
     public Payment cancelPayment(PaymentCancelRequestDto cancelDto, Long userId) throws IamportResponseException, IOException {
         Payment payment = paymentRepository.findByImpUid(cancelDto.getImpUid())
                 .orElseThrow(() -> new CustomException(ErrorCode.PAYMENT_NOT_FOUND));
@@ -82,13 +51,9 @@ public class PaymentService {
             throw new CustomException(ErrorCode.UNAUTHORIZED_ACTION);
         }
 
-        CancelData cancelData = new CancelData(cancelDto.getImpUid(), true); // true: 전액 환불
-        cancelData.setReason(cancelDto.getReason());
+        IamportResponse<com.siot.IamportRestClient.response.Payment> iamportResponse = paymentClient.cancelPaymentByImpUid(cancelDto);
 
-        IamportResponse<com.siot.IamportRestClient.response.Payment> iamportResponse = iamportClient.cancelPaymentByImpUid(cancelData);
-
-        payment.changeStatus(iamportResponse.getResponse().getStatus());
-        return paymentRepository.save(payment);
+        return paymentTransactionService.updatePaymentStatusToCancelled(iamportResponse.getResponse());
     }
 
     @Transactional(readOnly = true)
@@ -101,4 +66,4 @@ public class PaymentService {
     public List<Payment> getMyPayments(Long userId) {
         return paymentRepository.findAllByUserId(userId);
     }
-} 
+}
