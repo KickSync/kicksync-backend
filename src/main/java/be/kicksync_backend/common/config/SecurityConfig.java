b322e5459ab7c843496a57b7177ff7e6e6147577
gminnimk <tvt1245@naver.com>
2026-01-15 22:11:18 +0900

    feat: Swagger

--- src/main/java/be/kicksync_backend/common/config/SecurityConfig.java
+++ src/main/java/be/kicksync_backend/common/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/users/signup", "/api/users/login").permitAll()
                         .requestMatchers("/api/admin/signup", "/api/admin/login").permitAll()
                         .requestMatchers("/api/token/refresh").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
